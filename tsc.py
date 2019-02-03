1c7b4c240eb6ba4e871ae6457af09837fdc792e9
Thomas Step <thomasstep@outlook.com>
2019-02-03 11:30:28 -0600

    Added login rpc.

--- tsc.py
+++ tsc.py
@@ -11,6 +11,10 @@ channel = grpc.insecure_channel(sys.argv[1] + ":" + sys.argv[2])
 # Creating a grpc stub
 stub = tsns_pb2_grpc.TinySocialNetworkServiceStub(channel)
 
+# Logging in
+login = tsns_pb2.Login(Username="thomas", Password="me", LoggedIn=False)
+stub.Login(login)
+
 # Creating a follow request
 follow = tsns_pb2.ToggleFollow(Origin="thomas", Target="shawn", Following=False)
 
